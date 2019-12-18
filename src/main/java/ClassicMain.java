e86bd5b5411af133f8cc1cd4fd810458beeda4ee
Benjamin Ledel <benjamin@schule-plus.com>
2019-12-18 19:11:31 +0100

    * simple null pointer

---
+++ src/main/java/ClassicMain.java
@@ -0,0 +1,9 @@
+public class ClassicMain {
+
+    public static void main(String args[])
+    {
+        SimpleObject object = null;
+        System.out.printf(object.toString());
+        object = new SimpleObject();
+    }
+}
